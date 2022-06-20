632ae3659126e6b47584801ecf4a6d6b86257319
gustavocss <gcarvalhaesdes00@mylangara.ca>
2022-06-20 12:35:14 -0800

    Added simple test case for querying database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {db} from "../db/db.js"
 import path, {dirname} from "path";
+import { Prediction } from "../db/models/prediction.js";
 
 const app = express();
 
@@ -11,6 +12,20 @@ app.use(express.urlencoded({extended: true}));
 db.on("open", () => {
     console.log("Connected to database!");
     const server = app.listen(process.env.PORT || 3000, () => console.log("Listening..."));
+})
+
+// Testing purposes:
+
+app.get("/", (req, res) => {
+    Prediction.find({})
+        .exec()
+        .then((item) => {
+            res.status(200).json(item)
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).json(error);
+        })
 })
 
 // const router = require(path.join(__dirname, routes))

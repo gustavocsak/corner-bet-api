77c5e7e1afb82363449ff6475a7e877297bcea1d
gustavocss <gcarvalhaesdes00@mylangara.ca>
2022-06-20 11:44:48 -0800

    Added connection to Matches database

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-import express from 'express';
+import express from "express";
+import {db} from "../db/db.js"
+import path, {dirname} from "path";
+const app = express();
+
+app.set("json spaces", 40);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+db.on("open", () => {
+    console.log("Connected to database!");
+    const server = app.listen(process.env.PORT || 3000, () => console.log("Listening..."));
+})
+
+// const router = require(path.join(__dirname, routes))
